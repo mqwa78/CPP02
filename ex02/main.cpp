f1d31a416423a0b59a670a33a823a65a73525670
mqwa <mqdev78@gmail.com>
2025-05-26 22:57:58 +0200

    CPP02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,32 +6,56 @@
 /*   By: mqwa <mqwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 21:46:32 by mqwa              #+#    #+#             */
-/*   Updated: 2025/05/14 20:44:12 by mqwa             ###   ########.fr       */
+/*   Updated: 2025/05/23 16:46:00 by mqwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Fixed.hpp"
 
-int		main( void ) {
+int		main(void)
+{
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+	Fixed const c(Fixed(3.5f) + Fixed(1.5f));
+	Fixed d(10);
+	Fixed e(2);
 
-	Fixed			a;
-	Fixed const		b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << "\n--- Valeurs initiales ---\n";
+	std::cout << "a: " << a << std::endl;
+	std::cout << "b: " << b << std::endl;
+	std::cout << "c: " << c << std::endl;
+	std::cout << "d: " << d << std::endl;
+	std::cout << "e: " << e << std::endl;
 
-	std::cout << a << std::endl;
-	std::cout << b / a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "\n--- Arithmétique ---\n";
+	std::cout << "d + e = " << (d + e) << std::endl;
+	std::cout << "d - e = " << (d - e) << std::endl;
+	std::cout << "d * e = " << (d * e) << std::endl;
+	std::cout << "d / e = " << (d / e) << std::endl;
 
-	std::cout << b << std::endl;
+	std::cout << "\n--- Comparaisons ---\n";
+	std::cout << "d > e: " << (d > e) << std::endl;
+	std::cout << "d < e: " << (d < e) << std::endl;
+	std::cout << "d == e: " << (d == e) << std::endl;
+	std::cout << "d != e: " << (d != e) << std::endl;
+	std::cout << "d >= e: " << (d >= e) << std::endl;
+	std::cout << "d <= e: " << (d <= e) << std::endl;
 
-	std::cout << Fixed::max( a, b ) << std::endl;
-	if (a >= b)
-		std::cout << "a is greater than b" << std::endl;
-	if (a <= b)
-		std::cout << "a is less than b" << std::endl;
-	if (a == b)
-		std::cout << "a is equal to b" << std::endl;
-	return (0);
+	std::cout << "\n--- Incrémentation/Décrémentation ---\n";
+	std::cout << "a (init): " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "--a: " << --a << std::endl;
+	std::cout << "a--: " << a-- << std::endl;
+	std::cout << "a: " << a << std::endl;
+
+	std::cout << "\n--- Fonctions min / max ---\n";
+	std::cout << "min(d, e): " << Fixed::min(d, e) << std::endl;
+	std::cout << "max(d, e): " << Fixed::max(d, e) << std::endl;
+	std::cout << "min(b, c): " << Fixed::min(b, c) << std::endl;
+	std::cout << "max(b, c): " << Fixed::max(b, c) << std::endl;
+
+	return 0;
 }
